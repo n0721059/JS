155b1a5ebe14e3ab11a39b1d8a24fc1a9b30ce6e
n0721059 <muhamad.abumuch@gmail.com>
2023-09-19 15:36:12 +0300

    loops

---
+++ Arrays/7.3simple_slice.js
@@ -0,0 +1,4 @@
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+
+const citrusFruits = fruits.slice(1,3);
+console.log(citrusFruits);
---
+++ Arrays/7.4simple_splice.js
@@ -0,0 +1,7 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+//remove 0 elements at index and add lemon kiwi and 
+fruits.splice(2, 0, "Lemon", "Kiwi");
+
+
+console.log(fruits);
